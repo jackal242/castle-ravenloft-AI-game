b2d8d4b9292e126f63e0e6369054c2b42515ce4c
jackal242 <brad.allison@gmail.com>
2025-04-13 22:51:53 -0400

    adding stuff

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/jackal/play/castle-ravenloft-game/myenv
+set -gx VIRTUAL_ENV /Users/jackal/play/castle-ravenloft-AI-game/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
